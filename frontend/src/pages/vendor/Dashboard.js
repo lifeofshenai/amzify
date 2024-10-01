24e715d17b9eee68b073e5a88fafa78ad89517e7
Adamu Muhammad Muhammad <muhdadamu0008@gmail.com>
2024-10-01 03:51:41 +0100

    feat: vendor page

--- frontend/src/pages/vendor/Dashboard.js
+++ frontend/src/pages/vendor/Dashboard.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import DashboardStats from "../../components/DashboardStats";
+import SaleSummary from "../../components/SaleSummary";
+import TopProductsTable from "../../components/TopProductsTable";
+
 
 const VendorDashboard = () => {
   return (
-    <div className="p-4">
-      <h1>VendorDashboard</h1>
-      {/* Dashboard content goes here */}
+    <div>
+      {/* Dashboard Stats */}
+      <DashboardStats />
+      <SaleSummary />
+      <TopProductsTable />
+      
+      {/* Orders Table */}
+      {/* <OrdersTable /> */}
+      
+      {/* Top Products Table */}
+      {/* <TopProductsTable /> */}
     </div>
   );
-}
+};
 
-export default VendorDashboard
+export default VendorDashboard;
