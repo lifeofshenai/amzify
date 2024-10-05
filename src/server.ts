b3ef774b53049a32b42ae8e2c4877ba82cc6bb3d
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2024-10-05 08:52:47 +0100

    chore: cors refactored

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,8 @@ export function createServer(): Application {
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin"],
   };
 
   app.use(express.urlencoded({extended: false}));
